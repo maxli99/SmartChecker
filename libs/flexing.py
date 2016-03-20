61bc08c2ad24d811f963da2d910c341110cc3b35
Jun Liu <liujun.gz@gmail.com>
2016-03-20 16:12:37 +0800

    add InfoCache in libs/tools.py for sharing info/data in global

--- libs/flexing.py
+++ libs/flexing.py
@@ -1,17 +1,29 @@
 import re
 from hwparser import HardwareInfo
 
+class FlexiNG(object):
+    pass
+
+class FlexiNGVersion(object):
+    pass
+    
 def get_ng_version(configlog):
     """This function will parse the NG version info from logfile.
-    Input    config logfile
+parameters:
+    configlog    config logfiler
+return:
     return  a tuple include (major_version, release_version, hardware_version)
+    
     example: ('3.2', '123445', '2')
+    
     """
     version = None
     version_pat = re.compile("_NG([\d\._]+)_r(\d+)_AB(\d+)")
-
+    if not configlog:
+        return None
     try:
-        log = ''.join(file(configlog).readlines())
+        lines=file(configlog).readlines()
+        log = ''.join(lines)
     except IOError,e:
         print("IOError: %s" % e)
         exit(1)
